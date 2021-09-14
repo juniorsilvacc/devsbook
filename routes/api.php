69ee52e27291cd5b6d4b92f94929d6138102beb5
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-09-14 18:17:07 -0300

    creating controllers

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get("/ping", function () {
     return ['pong' => true];
 });
+
+Route::get('/401', 'AuthController@unauthorized')->name('login');
+
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/auth/logout', 'AuthController@logout');
+Route::post('/auth/refresh', 'AuthController@refresh');
+
+Route::post('/user', 'AuthController@create');
+Route::put('/user', 'UserController@update');
+Route::post('/user/avatar', 'UserController@updateAvatar');
+Route::post('/user/cover', 'UserController@updateCover');
+
+Route::get('/feed', 'FeedController@refresh');
+Route::get('/user/feed', 'FeedController@refresh');
+Route::get('/user/{id}/feed', 'FeedController@refresh');
+
+Route::get('/user', 'FeedController@read');
+Route::get('/user/{id}', 'FeedController@read');
+
+Route::post('/feed', 'FeedController@create');
+
+Route::post('/post/{id}', 'PostController@like');
+Route::post('/post/{id}/{comment}', 'PostController@comment');
+
+Route::get('/search', 'SearchController@search');
