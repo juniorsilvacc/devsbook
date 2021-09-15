1a9c087a83a78f27f6bdca3d4ed0d8b9a51056b5
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-09-15 00:33:04 -0300

    creating users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,11 +4,65 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
+
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login', 'create', 'unauthorized']]);
+    }
+
+
+    public function create(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $birthdate = $request->input('birthdate');
+
+        if ($name && $email && $password && $birthdate) {
+            //Validando a data de nascimento
+            if (strtotime($birthdate) === false) {
+                $array['error'] = 'Data de nascimento inválido.';
+                return $array;
+            }
+
+            //Verificar se existe o e-mail
+            $emailExists = User::where('email', $email)->count();
+            if ($emailExists === 0) {
+
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                $newUser = new User();
+                $newUser->name = $name;
+                $newUser->email = $email;
+                $newUser->password = $hash;
+                $newUser->birthdate = $birthdate;
+                $newUser->save();
+
+                $token = auth::attempt([
+                    'email' => $email,
+                    'password' => $password
+                ]);
+                if (!$token) {
+                    $array['error'] = 'Ocorreu um erro';
+                    return $array;
+                }
+
+                $array['token'] = $token;
+            } else {
+                $array['error'] = 'E-mail já cadastrado.';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Não enviou todos os campos.';
+            return $array;
+        }
+
+        return $array;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,15 @@ Route::get("/ping", function () {
     return ['pong' => true];
 });
 
+/*
 Route::get('/401', 'AuthController@unauthorized')->name('login');
 
 Route::post('/auth/login', 'AuthController@login');
 Route::post('/auth/logout', 'AuthController@logout');
 Route::post('/auth/refresh', 'AuthController@refresh');
-
+*/
 Route::post('/user', 'AuthController@create');
+/*
 Route::put('/user', 'UserController@update');
 Route::post('/user/avatar', 'UserController@updateAvatar');
 Route::post('/user/cover', 'UserController@updateCover');
@@ -42,3 +44,4 @@ Route::post('/post/{id}', 'PostController@like');
 Route::post('/post/{id}/{comment}', 'PostController@comment');
 
 Route::get('/search', 'SearchController@search');
+*/
