907ab7bfd2d9bdeaebf323fba42bbfd22b6f01fd
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-10-02 02:47:56 -0300

    list photos

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -205,4 +205,43 @@ class FeedController extends Controller
 
         return $postList;
     }
+
+    public function userPhotos(Request $request, $id = false)
+    {
+        $array = ['error' => ''];
+
+        //Quando for false mando usuário que está logado
+        if ($id == false) {
+            $id = $this->loggedUser['id'];
+        }
+
+        $page = intval($request->input('page'));
+        $perPage = 2;
+
+        //Pegar as fotos do usuários ordenado pela data
+        $postList = Post::where('id_user', $id)
+            ->where('type', 'photo')
+            ->orderBy('created_at', 'desc')
+            ->offset($page * $perPage)
+            ->limit($perPage)
+            ->get();
+
+        $total = Post::where('id_user', $id)
+            ->where('type', 'photo')
+            ->count();
+        $pageCount = ceil($total / $perPage);
+
+        //Preencher as informações adicionais
+        $posts = $this->_postListToObject($postList, $this->loggedUser['id']);
+
+        foreach ($posts as $pkey => $post) {
+            $posts[$pkey]['body'] = url('media/uploads/' . $posts[$pkey]['body']);
+        }
+
+        $array['posts'] = $posts;
+        $array['pageCount'] = $pageCount;
+        $array['currentPage'] = $page;
+
+        return $array;
+    }
 }
